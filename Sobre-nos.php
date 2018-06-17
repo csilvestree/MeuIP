586a632914b5c5cac159018451eae7feb6c30926
Cauê <cauesilvestree@hotmail.com>
2018-06-17 19:24:39 -0300

    Criação da estrutura das páginas Sobre
    Foi feito o molde/estrutura das páginas Sobre nós e Sobre o IP

---
+++ Sobre-nos.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="assets/images/favicon.ico">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
+  <link rel="stylesheet" href="assets/css/theme.css">
+  <title>Oque é um IP? | Meu IP</title>
+  <meta name="description" content="Oque é um endereço IP? Descubra agora mesmo com quem entende do assunto!">
+  <meta name="keywords" content="qual meu ip, meu ip, qual o meu ip, ip, endereço ip, o meu ip, meu ipe, como saber meu ip, ip internet, endereço de ip, meu ip local, como descobrir meu ip, meu endereço ip, descobrir ip, pesquisar ip, ip site, como ver meu ip, ip publico, meu ip localização, consulta ip, qual e o meu ip, descobrir meu ip">
+  <meta name="robots" content="index, follow">
+  <link rel="canonical" href="https://meuip.site/">
+  <!-- Mídias Sociais -->
+  <meta property="og:title" content="Meu IP | Qual o meu IP? Ver Agora">
+  <meta property="og:description" content="Descubra agora mesmo qual é o seu endereço IP e várias outras informações a respeito do seu IP Address como localização e provedor de internet.">
+  <meta property="og:image" content="https://i.imgur.com/2rKlVG6.png">
+  <meta property="og:locale" content="pt_BR">
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="https://meuip.site/">
+  <meta property="og:site_name" content="Meu IP | Qual o meu IP? Ver Agora">
+  <meta name="twitter:title" content="Meu IP | Qual o meu IP? Ver Agora">
+  <meta name="twitter:description" content="Descubra agora mesmo qual é o seu endereço IP e várias outras informações a respeito do seu IP Address como localização e provedor de internet.">
+  <meta name="twitter:url" content="https://meuip.site/">
+  <meta name="twitter:image" content="https://i.imgur.com/2rKlVG6.png">
+  <meta name="twitter:site" content="@MeuIpSite">
+  <meta name="twitter:creator" content="@MeuIpSite"> </head>
+
+<body>
+  <nav class="navbar navbar-expand-md navbar-light m-4">
+    <div class="container">
+      <a class="navbar-brand" href="#">
+        <img src="../../../Downloads/MeuIP/Logo.fw.png" height="40" class="d-inline-block align-top mx-5" alt="Descobrir meu IP" width="214"> </a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar3SupportedContent" aria-controls="navbar3SupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse text-center justify-content-center" id="navbar3SupportedContent">
+        <ul class="navbar-nav ml-auto text-primary">
+          <li class="nav-item active mx-3">
+            <a class="nav-link text-white" href="https://meuip.site/">
+              <span class="mx-3 text-dark">
+                <b>
+                  <b class="text-danger">Início</b>
+                </b>
+              </span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="Minecraft/">
+              <span class="text-white mx-3">
+                <b class="text-dark">
+                  <b class="text-danger">Sobre o IP</b>
+                </b>
+              </span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="Sites/">
+              <b class="mx-3 text-dark">
+                <b class="text-danger">Sobre nós</b>
+              </b>
+            </a>
+          </li>
+        </ul>
+        <ul class="navbar-nav"></ul>
+      </div>
+    </div>
+  </nav>
+  <div class="" style="min-height:500px;">
+    <div class="container my-5 py-5">
+      <div class="row">
+        <div class="col-md-12 p-4">
+          <div class="row">
+            <div class="col-sm-3 text-center">
+              <i class="d-block  fa fa-5x fa-address-card"></i>
+            </div>
+            <div class="col-sm-9">
+              <h3 class="">Quem somos?</h3>
+              <p class="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla elementum ligula diam, nec aliquet felis</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-9 col-md-12">
+              <p class="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla elementum ligula diam, nec aliquet felis mattis a. Suspendisse eros libero, elementum nec ligula in, dapibus dignissim est. Ut bibendum rhoncus purus fringilla pharetra. Integer
+                accumsan mauris mi, vitae ultricies leo suscipit et. Suspendisse vehicula non velit eget cursus. Mauris id turpis non sem sodales porttitor ac nec nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+                himenaeos. Duis laoreet mi faucibus metus laoreet vehicula. Proin dui quam, egestas non dui vitae, pulvinar facilisis metus.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="bg-dark text-white">
+    <div class="container">
+      <div class="row">
+        <div class="p-4 col-md-3">
+          <h2 class="mb-4 text-white">MeuIP.site</h2>
+          <p class="text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla elementum ligula diam, nec aliquet felis mattis a. </p>
+        </div>
+        <div class="p-4 col-md-3">
+          <h2 class="mb-4 text-white">Sitemap</h2>
+          <ul class="list-unstyled">
+            <a href="#" class="text-white">Início</a>
+            <br>
+            <a href="#" class="text-white">Sobre o IP</a>
+            <br>
+            <a href="#" class="text-white">Sobre nós</a>
+            <br> </ul>
+        </div>
+        <div class="p-4 col-md-2">
+          <h2 class="mb-4">Contato</h2>
+          <p class="text-white">
+            <a href="mailto:info@meuip.site" class="text-white">
+              <i class="fa d-inline mr-3 text-white fa-envelope-o"></i>i</a>nfo@meuip.site</p>
+          <p>
+            <i class="fa d-inline mr-3 fa-map-marker text-white"></i>&nbsp;Brazil</p>
+        </div>
+        <div class="p-4 col-md-4">
+          <div class="row my-5">
+            <div class="col-4 col-md-3 align-self-center">
+              <a href="https://www.facebook.com/Meuip-2288780434482248/" target="_blank">
+                <i class="fa fa-fw fa-facebook fa-3x text-white"></i>
+              </a>
+            </div>
+            <div class="col-4 col-md-3 align-self-center">
+              <a href="https://twitter.com/MeuIpSite" target="_blank">
+                <i class="fa fa-fw fa-twitter fa-3x text-white"></i>
+              </a>
+            </div>
+            <div class="col-4 col-md-3 align-self-center">
+              <a href="https://plus.google.com/103750553368971257826" target="_blank">
+                <i class="fa fa-fw text-white fa-3x fa-google-plus"></i>
+              </a>
+            </div>
+            <div class="col-4 col-md-3 align-self-center">
+              <a href="https://www.instagram.com" target="_blank">
+                <i class="fa fa-fw text-white fa-3x fa-youtube-play"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+
+</html>
